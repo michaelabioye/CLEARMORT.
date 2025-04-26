1d266b876b83433abe6c3b2fa29a2e9e7f882a89
michaelabioye <michaelabioye77@gmail.com>
2025-04-25 21:20:04 -0400

    javascript updates

--- js/site.js
+++ js/site.js
@@ -1,21 +1,49 @@
 function getValues() {
 
-    //get the <input/> element from html. you get the element by adding = 
-    // document to your javascript
-    let inputElement = document.getElementById('messageinput');
+    loanElement = document.getElementById('loanAmount');
+    termElement = document.getElementById('termMonths');
+    intrElement = document.getElementById('interestRate');
 
+    let loan = parseInt(loanElement.value);
+    let term = parseInt(termElement.value);
+    let inter = parseFloat(intrElement.value);
 
-    //then get the text out of the input element 
-    let message = inputElement.value;
+    if (loan, term, inter, '') {
 
-    //then use alert() to show the text that was typed
-    Swal.fire(
-        {
+        swal.fire({
             backdrop: false,
-            title: 'ClearMort.',
-            text: message,
-        }
-    )
+            title: 'uh oh',
+            text: 'Please Enter Amount To View Payments ',
+            icon: 'error'
+        })
+    } else if (Number.isInteger(loan) && Number.isInteger(term)
+        && !Number.isInteger(inter)) {
+
+        let summaryCalc = calcSummary(loan, term, inter);
+
+        displaySummary(summaryCalc);
+    }
+
+    /*The amount of money loaned in dollars (balance)
+The time over which the loan will be repaid, in months (term)
+The annual percentage rate at which interest will accrue on the loan (rate)*/
+
 
 }
 
+
+function calcSummary(loan, term, inter) {
+
+    (loan) * (inter / 1200) / (1 - (1 + inter / 1200) ^ (-term))
+
+
+
+    return calcSummary;
+
+}
+
+
+
+function displaySummary() {
+
+}
